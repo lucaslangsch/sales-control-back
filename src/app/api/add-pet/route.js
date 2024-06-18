b34c83a6972789e478769502d97b09662945867a
lucaslangsch <lucas.langsch@gmail.com>
2024-06-18 13:47:15 -0300

    feat: modify simple routes to post

--- src/app/api/add-pet/route.js
+++ src/app/api/add-pet/route.js
@@ -1,7 +1,7 @@
 import { sql } from '@vercel/postgres';
 import { NextResponse } from 'next/server';
  
-export async function GET(request) {
+export async function POST(request) {
   const { searchParams } = new URL(request.url);
   const petName = searchParams.get('petName');
   const ownerName = searchParams.get('ownerName');
